3cb88c546e78edd1d05170dff12dbb65880d7da3
sarath k s <code-freakz@users.noreply.github.com>
2020-08-17 20:17:35 +0530

    live api config change

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 const onProduction = process.env.NODE_ENV === 'production';
 
-export const BASE_URL = onProduction ? 'https://phantompod.co:4000/' : 'http://localhost:4000/';
+export const BASE_URL = onProduction ? 'https://app.phantompod.co/api/' : 'http://localhost:4000/';
 export const AUTH_TOKEN_KEY = 'admintoken';
